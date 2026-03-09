bbcb61525cd4dd32454ae831d4bb14fa095e619a
VeeraReddyRavuri <veerareddy.ravuri01@gmail.com>
2026-03-09 09:30:17 +0000

    Add memory usage monitoring and console logging handler

--- toolkit/main.py
+++ toolkit/main.py
@@ -1,6 +1,6 @@
 import yaml
 import logging
-from toolkit.monitor import check_cpu
+from toolkit.monitor import check_cpu, check_memory
 
 def setup_logger():
     logger = logging.getLogger(__name__)
@@ -12,7 +12,12 @@ def setup_logger():
 
     file_handler = logging.FileHandler("logs/toolkit.log")
     file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
     logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
     return logger
 
@@ -32,6 +37,13 @@ def main():
 
     if cpu_result["status"] == "Alert":
         logger.warning("CPU usage exceeded threshold")
+
+    memory_result = check_memory(config["memory_threshold"])
+
+    logger.info(f"Memory usage: {memory_result["memory_usage"]}%")
+
+    if memory_result["stauts"] == "Alert":
+        logger.warning("Memory usage exceeding threshold")
 
 
 if __name__ == "__main__":
--- toolkit/monitor.py
+++ toolkit/monitor.py
@@ -13,3 +13,16 @@ def check_cpu(threshold):
         "cpu_usage": cpu_usage
     }
     
+def check_memory(threshold):
+    memory = psutil.virtual_memory()
+    memory_usage =  memory.percent
+
+    if memory_usage > threshold:
+        return {
+            "stauts": "Alert",
+            "memory_usage": memory_usage
+        }
+    return {
+            "stauts": "OK",
+            "memory_usage": memory_usage
+    }
